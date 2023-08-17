61430a9e7ec984785558bdded349a411ca7a9788
Prabeez <141338491+Prabeez@users.noreply.github.com>
2023-08-17 13:07:38 +1000

    pracctical

---
+++ array/array/array.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SubjectArrayExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an array of subjects
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            Console.WriteLine(cars[0]);
+            
+
+        }
+    }
+}
